6bc11301646940c0f7611d00414db20e6d8f3b54
Lucas Souza <Lucas Souza>
2021-05-11 17:35:17 -0300

    Component ng serve

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MyFirstComponent } from './myfirst.component';
 import { MySecondComponent } from './my-second/my-second.component';
+import { MyThirdComponent } from './my-second/my-third/my-third.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyFirstComponent,
-    MySecondComponent
+    MySecondComponent,
+    MyThirdComponent
   ],
   imports: [
     BrowserModule
