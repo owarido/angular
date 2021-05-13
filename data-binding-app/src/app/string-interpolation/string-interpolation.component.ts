db88c7d8131cd9ddf46a9b9959165b2a3b3c36ca
Lucas Souza <Lucas Souza>
2021-05-13 17:03:21 -0300

    String Interpolation

---
+++ data-binding-app/src/app/string-interpolation/string-interpolation.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-string-interpolation',
+  templateUrl: './string-interpolation.component.html',
+  styleUrls: ['./string-interpolation.component.css']
+})
+export class StringInterpolationComponent implements OnInit {
+
+  firstname = "John";
+  age = 100;
+
+  person = {
+    firstname: "John",
+    lastname: "bro",
+    age: 50,
+    address: "Route 100"
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
